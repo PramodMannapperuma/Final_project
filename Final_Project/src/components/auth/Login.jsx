612e413172b88157496446ba90230437fba3562c
Pramod Mannapperuma <96408353+PramodMannapperuma@users.noreply.github.com>
2024-01-12 04:39:11 +0530

    Added auth pages

--- Final_Project/src/components/auth/Login.jsx
+++ Final_Project/src/components/auth/Login.jsx
@@ -1,11 +1,43 @@
+import React from "react";
+import { FaUser, FaLock } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import './Form.css'
 const Login = () => {
-    return ( 
-        <div>
-            <h1>
-                Login
-            </h1>
+    return (
+        <div className="container1">
+            <div className="wrapper">
+                <form>
+                    <h1>Login</h1>
+                    <div className="input-box">
+                        <input
+                            type="text"
+                            placeholder="Email"
+                        
+                        />
+                        <FaUser className="icon" />
+                    </div>
+                    <div className="input-box">
+                        <input
+                            type="password"
+                            placeholder="Password"
+   
+                            
+                        />
+                        <FaLock className="icon" />
+
+                    </div>
+                    <button type="submit">Login</button>
+
+                    <div className="register-link">
+                        <p>
+                            Don&apos;t have an account ?{" "}
+                            <Link to="/signup">Register</Link>
+                        </p>
+                    </div>
+                </form>
+            </div>
         </div>
-     );
-}
- 
+    );
+};
+
 export default Login;
